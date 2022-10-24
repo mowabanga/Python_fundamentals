4c955224d00ae9b5a8366257ddd96bdc46ad29e0
mowabanga <itsmundia@gmail.com>
2022-10-24 12:30:46 +0300

    new

---
+++ py101/aliens.py
@@ -0,0 +1,7 @@
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+    print(f"You just earned 5 points!")
+elif 'yellow' in alien_color:
+    print(f"You just earned 10 points!")
+elif 'red' in alien_color:
+    print(f"You just earned 15 points!")
